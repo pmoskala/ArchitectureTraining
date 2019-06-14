fe5f25ade78306cd2e62be848ffa347fa368f234
Piotr Moskała <moskala.p@eplan.pl>
2019-06-14 13:37:30 +0200

    Query impl

--- src/SFC.AdminApi/Dashboard/DashboardPerspective.cs
+++ src/SFC.AdminApi/Dashboard/DashboardPerspective.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using SFC.Accounts.Features.AccountQuery.Contract;
+using SFC.Infrastructure;
+using SFC.Notifications.Features.NotificationQuery;
+using SFC.Notifications.Features.NotificationQuery.Contract;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SFC.AdminApi.Dashboard
 {
-  class DashboardPerspective : IDashboardPerspective
-  {
-    public DashboardResult Search(DashboardQueryModel query)
+    class DashboardPerspective : IDashboardPerspective
     {
-      // implement gathering data from other services
+        private readonly IQuery _query;
 
-      List<DashboardEntry> entries = new List<DashboardEntry>();
+        public DashboardPerspective(IQuery query)
+        {
+            _query = query;
+        }
+        public DashboardResult Search(DashboardQueryModel query)
+        {
+            AccountsReadModel result = _query.Query<AccountsReadModel, AccountQuery>(new AccountQuery
+            {
+                Skip = query.Top,
+                Take = query.Take
+            });
 
-      return new DashboardResult(entries);
+            IEnumerable<NotificationsCountResult> counts = _query.Query<IEnumerable<NotificationsCountResult>, NotificationsCountRequest>(
+                new NotificationsCountRequest(result.Accounts.Select(x => x.LoginName).ToArray()));
+
+            IEnumerable<DashboardEntry> dashboardEntries = counts.Select(x => new DashboardEntry
+            {
+                LoginName = x.LoginName,
+                EmailsSentCount = x.Count
+            });
+
+            return new DashboardResult(dashboardEntries);
+        }
     }
-  }
 }
